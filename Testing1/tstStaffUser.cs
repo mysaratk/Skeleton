66da7b6d8c40422108de1b555cd434cb65d56000
Mysaratk <p2678181@my365.dmu.ac.uk>
2024-06-04 17:54:25 +0100

    page12

---
+++ Testing1/tstStaffUser.cs
@@ -0,0 +1,71 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaffUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaffUser AnUser = new clsStaffUser();
+            Assert.IsNotNull(AnUser);
+        }
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            clsStaffUser AnUser = new clsStaffUser();
+            Int32 TestData = 1;
+            AnUser.UserID = TestData;
+            Assert.AreEqual(AnUser.UserID, TestData);
+        }
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffUser AnUser = new clsStaffUser();
+            //create some test data to assign to the property
+            string TestData = "Piotr";
+            //assign the data to the property
+            AnUser.UserName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffUser AnUser = new clsStaffUser();
+            //create some test data to assign to the property
+            string TestData = "123m";
+            //assign the data to the property
+            AnUser.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffUser AnUser = new clsStaffUser();
+            //create some test data to assign to the property
+            string TestData = "yyy";
+            //assign the data to the property
+            AnUser.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+        [TestMethod]
+        public void FindUserMethodOK() 
+        {
+            clsStaffUser AnUser = new clsStaffUser();
+            Boolean Found = false;
+            string UserName = "Piotr";
+            string Password = "123p";
+            Found = AnUser.FindUser(UserName, Password);
+            Assert.IsTrue(Found);
+        }
+    }
+}
