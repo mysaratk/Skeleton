89c67d58cd0f04b494e316a3c86f40e73a287cfb
Piotr Wasowicz <P2692692@my365.dmu.ac.uk>
2024-06-05 16:40:37 +0100

    Created one test for each attribute in UserAccount table schema. 100% works

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public short PhoneNumber { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int CustomerId { get; set; }
     }
 }
