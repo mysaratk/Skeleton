9896c72705c4c333de8071c2b760dc58d1b264a1
Borislav Dzhoganski <P2675367@my365.dmu.ac.uk>
2024-06-13 21:04:06 +0100

    Added tests in tstOrder

--- Testing5/tstOrder.cs
+++ Testing5/tstOrder.cs
@@ -13,5 +13,50 @@ namespace Testing5
             clsOrder AnOrder = new clsOrder(); 
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void DlvrAddressOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "33 Castle Street";
+            AnOrder.DlvrAddress = TestData;
+            Assert.AreEqual(AnOrder.DlvrAddress, TestData);
+        }
+
+        [TestMethod]
+        public void Order8OK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.OrderD8 = TestData;
+            Assert.AreEqual(AnOrder.OrderD8, TestData);
+        }
+
+        [TestMethod]
+        public void TtlAmountOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 TestData = 1;
+            AnOrder.TtlAmount = TestData;
+            Assert.AreEqual(AnOrder.TtlAmount, TestData);
+        }
+
+        [TestMethod]
+        public void OrdrPaidOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.OrdrPaid = TestData;
+            Assert.AreEqual(AnOrder.OrdrPaid, TestData);
+        }
+
+        [TestMethod]
+        public void SortedOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.Sorted = TestData;
+            Assert.AreEqual(AnOrder.Sorted, TestData);
+        }
     }
 }
